f63bc4806111d95f360bdeafa92bf72c80eca570
Kae Benton <sonja.k.benton@gmail.com>
2020-08-14 15:46:34 -0400

    Added properties to Character component.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,15 +1,29 @@
 // Write your Character component here
-import React from 'react';
+import React, {useState} from 'react';
 // import axios from 'axios';
 import styled from 'styled-components';
 
 export default function Character(props) {
     const {character} = props
+    const [isDetailsOn, setIsDetailsOn ] = useState(false)
+
+    const expandDetails = evt => {
+        setIsDetailsOn(!isDetailsOn)
+    }
 
     return (
         <div className='character container'>
             <h2>{character.name}</h2>
-            <p>{character.height}</p>
+            <div>
+            <p>Height: {character.height}</p>
+            <p>Mass(kg): {character.mass}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Skin Color: {character.skin_color}</p>
+            <p>Eye Color: {character.eye_color}</p>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+            </div>
+           
         </div>
 
     )
